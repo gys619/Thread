22ec23afe7ee48442e4e4958cb5d62fab7cd668f
kangwenhang <kangwenhang@hotmail.com>
2021-08-29 23:28:40 +0800

    update

--- shell/run.sh
+++ shell/run.sh
@@ -3,8 +3,8 @@
 ## 导入通用变量与函数
 config=$dir_root/config
 shell=$dir_root/shell
-. file_config=$config/config"$1".sh
-. file_push=$shell/push.sh
+. ./file_config=$config/config"$1".sh
+. ./file_push=$shell/push.sh
 
 ##运行脚本
 exec file_push
