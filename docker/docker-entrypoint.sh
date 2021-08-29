1f2fcb6a06ab066ac4ae608ef3d10f38fb58e1ef
kangwenhang <kangwenhang@hotmail.com>
2021-08-29 17:39:06 +0800

    update

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,7 +9,7 @@ then
   echo -e "成功添加定时任务...\n"
 else
   echo -e "检测到config配置目录下不存在crontab.list或存在但文件为空，从示例文件复制一份用于初始化...\n"
-  cp -fv ${dir_root}/sample/crontab.list.sample ${JD_DIR}/config/crontab.list
+  cp -fv ${dir_root}/sample/crontab.list.sample ${dir_root}/config/crontab.list
   echo
   crontab ${dir_root}/config/crontab.list
   echo -e "成功添加定时任务...\n"
@@ -17,7 +17,7 @@ fi
 
 if [ ! -s ${dir_root}/config/config.sh ]; then
   echo -e "检测到config配置目录下不存在config.sh，从示例文件复制一份用于初始化...\n"
-  cp -fv ${dir_root}/sample/config.sh.sample ${JD_DIR}/config/config.sh
+  cp -fv ${dir_root}/sample/config.sh.sample ${dir_root}/config/config.sh
   echo
 fi
 
