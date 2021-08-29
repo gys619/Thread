cdc46b088e2138f311eb8f11997a760181c91ffd
kangwenhang <kangwenhang@hotmail.com>
2021-08-30 01:23:00 +0800

    update

--- shell/run.sh
+++ shell/run.sh
@@ -2,9 +2,9 @@
 
 ## 导入通用变量与函数
 shell=/$dir_root/shell
-. /$shell/share.sh
-. /$shell/push.sh
-. /$config/config"$1".sh
+. $shell/share.sh
+. $shell/push.sh
+. $config/config"$1".sh
 
 ##运行脚本
 exec push
