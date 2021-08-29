47d6f8937fe22d295cfb53c184417ad94a3c719d
kangwenhang <kangwenhang@hotmail.com>
2021-08-30 01:19:04 +0800

    update

--- shell/share.sh
+++ shell/share.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bashconfig=$dir_root/config
 
 ##目录
+config=$dir_root/config
 shell=$dir_root/shell
 logs=$dir_root/logs
-diy_file=$$dir_root/diy
+diy_file=$dir_root/diy
 
 ##文件
 ListCron=$config/crontab.list
