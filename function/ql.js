a66a7afda6d52628dfcf63447bebe57405ba1c09
gys619 <1554537386@qq.com>
2021-12-02 17:05:24 +0800

    修改内容：[ function/ql.js]

--- function/ql.js
+++ function/ql.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 const { readFile } = require('fs/promises');
 const path = require('path');
 
-const qlDir = process.env.QL_DIR || '/ql';
+const qlDir = '/ql';
 const authFile = path.join(qlDir, 'config/auth.json');
 
 const api = got.extend({
-  prefixUrl: process.env.QL_URL || 'http://localhost:5600',
+  prefixUrl: 'http://127.0.0.1:5600',
   retry: { limit: 0 },
 });
 
@@ -110,6 +110,26 @@ module.exports.EnableCk = async (eid) => {
     },
   }).json();
   return body;
+};
+
+module.exports.getstatus = async (eid) => {
+  const envs = await this.getEnvs();
+  for (let i = 0; i < envs.length; i++) {	 
+	if(envs[i]._id==eid){
+		 return envs[i].status; 
+	  }
+  }  
+  return 99;
+};
+
+module.exports.getEnvById = async (eid) => {
+  const envs = await this.getEnvs();
+  for (let i = 0; i < envs.length; i++) {	 
+	if(envs[i]._id==eid){		 
+		 return envs[i].value; 
+	  }
+  }  
+  return "";
 };
 
 module.exports.delEnv = async (eid) => {
