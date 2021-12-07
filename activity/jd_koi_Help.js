4783b1d479f3718cc299b1b9a90f86a0acde0771
gys619 <1554537386@qq.com>
2021-12-07 23:09:45 +0800

    删除：[ activity/jd_angryKoi.js] 修改文件名：[ jd_koi_Help.js -> activity/jd_koi_Help.js]

--- activity/jd_koi_Help.js
+++ activity/jd_koi_Help.js
@@ -6,14 +6,14 @@
 ============Quantumultx===============
 [task_local]
 #锦鲤红包互助
-1 0,20 * * * https://raw.githubusercontent.com/444444/JDJB/main/jd_koi_Help.js, tag=锦鲤红包互助, enabled=true
+1 0,20 * * * https://raw.githubusercontent.com/KingRan/JDJB/main/jd_koi_Help.js, tag=锦鲤红包互助, enabled=true
 ================Loon==============
 [Script]
-cron "1 0,20 * * *" script-path=https://raw.githubusercontent.com/444444/JDJB/main/jd_koi_Help.js, tag=锦鲤红包互助
+cron "1 0,20 * * *" script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_koi_Help.js, tag=锦鲤红包互助
 ===============Surge=================
-锦鲤红包互助 = type=cron,cronexp="1 0,20 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/444444/JDJB/main/jd_koi_Help.js
+锦鲤红包互助 = type=cron,cronexp="1 0,20 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_koi_Help.js
 ============小火箭=========
-锦鲤红包互助 = type=cron,script-path=https://raw.githubusercontent.com/444444/JDJB/main/jd_koi_Help.js, cronexpr="1 0,20 * * *", timeout=3600, enable=true
+锦鲤红包互助 = type=cron,script-path=https://raw.githubusercontent.com/KingRan/JDJB/main/jd_koi_Help.js, cronexpr="1 0,20 * * *", timeout=3600, enable=true
 */
 
 const $ = new Env("锦鲤红包互助")
