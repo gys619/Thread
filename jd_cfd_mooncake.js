eb2850bb300a5d9028c87e28b143be13c2e3f232
gys619 <1554537386@qq.com>
2021-12-14 10:13:56 +0800

    修改内容：[ jd_cfd.js, jd_cfd_loop.js, jd_cfd_mooncake.js, jd_dreamFactory.js, jd_jxmc.js, jx_sign.js]

--- jd_cfd_mooncake.js
+++ jd_cfd_mooncake.js
@@ -782,7 +782,7 @@ async function requestAlgo() {
       "expandParams": ""
     })
   }
-  new Promise(async resolve => {
+  return new Promise(async resolve => {
     $.post(options, (err, resp, data) => {
       try {
         if (err) {
