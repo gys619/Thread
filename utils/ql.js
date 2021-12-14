531dc4fe73c9d6114ca87c8c03a4180de876228c
gys619 <1554537386@qq.com>
2021-12-14 20:18:51 +0800

    新增：[ jd_help_sendBean.js] 修改内容：[ function/ql.js, jd_jfcz.js, utils/ql.js]

--- utils/ql.js
+++ utils/ql.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 const { readFile } = require('fs/promises');
 const path = require('path');
 
-const qlDir = process.env.QL_DIR || '/ql';
+const qlDir = '/ql';
 const authFile = path.join(qlDir, 'config/auth.json');
 
 const api = got.extend({
-  prefixUrl: process.env.QL_URL || 'http://localhost:5600',
+  prefixUrl: 'http://localhost:5600',
   retry: { limit: 0 },
 });
 
@@ -120,6 +120,16 @@ module.exports.getstatus = async (eid) => {
 	  }
   }  
   return 99;
+};
+
+module.exports.getEnvById = async (eid) => {
+  const envs = await this.getEnvs();
+  for (let i = 0; i < envs.length; i++) {	 
+	if(envs[i]._id==eid){		 
+		 return envs[i].value; 
+	  }
+  }  
+  return "";
 };
 
 module.exports.delEnv = async (eid) => {
