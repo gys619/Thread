62d54b8239d68b1450cff1da68914b00d4703815
gys619 <1554537386@qq.com>
2021-12-30 23:10:30 +0800

    删除：[ gua_opencard87.js, gua_opencard89.js] 修改内容：[ jd_openCard.py]

--- jd_openCard.py
+++ jd_openCard.py
@@ -6,7 +6,7 @@ Author: Curtin
 功能：JD入会开卡领取京豆
 CreateDate: 2021/5/4 下午1:47
 UpdateTime: 2021/6/19
-建议cron: 2 8,15 * * *  python3 jd_OpenCard.py
+cron: 2 8,15 * * *  python3 jd_OpenCard.py
 new Env('开卡有礼');
 '''
 version = 'v1.2.2'
