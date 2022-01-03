b0f981f458dabb93088436c7ad6c9a06d6e6ee5c
gys619 <1554537386@qq.com>
2022-01-03 22:35:32 +0800

    修改内容：[ jd_hwmh.js, jd_m_sign.js]

--- jd_m_sign.js
+++ jd_m_sign.js
@@ -67,12 +67,15 @@ async function jdsign() {
     await $.wait(1000)
     await getInfo("https://pro.m.jd.com/mall/active/4QjXVcRyTcRaLPaU6z2e3Sw1QzWE/index.html");//全城购签到
     await $.wait(1000)
+    await getInfo("https://prodev.m.jd.com/mall/active/3MFSkPGCDZrP2WPKBRZdiKm9AZ7D/index.html");//同城签到
+    await $.wait(1000)
     await getInfo("https://pro.m.jd.com/mall/active/kPM3Xedz1PBiGQjY4ZYGmeVvrts/index.html");//陪伴
+    await $.wait(1000)
+    await getInfo("https://pro.m.jd.com/mall/active/3SC6rw5iBg66qrXPGmZMqFDwcyXi/index.html");//京东图书
 //     await getInfo("https://pro.m.jd.com/mall/active/ZrH7gGAcEkY2gH8wXqyAPoQgk6t/index.html");//箱包签到
 //     await $.wait(1000)
 //     await getInfo("https://pro.m.jd.com/mall/active/4RXyb1W4Y986LJW8ToqMK14BdTD/index.html");//鞋靴馆签到
-//     await $.wait(1000)
-//     await getInfo("https://pro.m.jd.com/mall/active/3SC6rw5iBg66qrXPGmZMqFDwcyXi/index.html");//京东图书
+
 //     await $.wait(1000)
 //     await getInfo("https://pro.m.jd.com/mall/active/3joSPpr7RgdHMbcuqoRQ8HbcPo9U/index.html");//生活特权签到
   } catch (e) {
