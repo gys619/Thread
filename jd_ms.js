319919739548594a412e3099cb48ee8e7673d3aa
gys619 <1554537386@qq.com>
2022-01-20 12:39:47 +0800

    修改内容：[ jd_ms.js]

--- jd_ms.js
+++ jd_ms.js
@@ -1,9 +1,25 @@
 /*
-
+京东秒秒币
+Last Modified time: 2021-05-22 8:55:00
+一天签到100币左右，100币可兑换1毛钱红包，推荐攒着配合农场一起用
+活动时间：长期活动
+更新地址：https://gitee.com/lxk0301/jd_scripts/raw/master/jd_ms.js
+活动入口：京东app-京东秒杀-签到领红包
+脚本兼容: QuantumultX, Surge, Loon, JSBox, Node.js
+============Quantumultx===============
 [task_local]
 #京东秒秒币
-10 7 * * * jd_ms.js
+10 7 * * * https://gitee.com/lxk0301/jd_scripts/raw/master/jd_ms.js, tag=京东秒秒币, img-url=https://raw.githubusercontent.com/yogayyy/Scripts/master/Icon/shylocks/jd_ms.jpg, enabled=true
 
+================Loon==============
+[Script]
+cron "10 7 * * *" script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_ms.js,tag=京东秒秒币
+
+===============Surge=================
+京东秒秒币 = type=cron,cronexp="10 7 * * *",wake-system=1,timeout=200,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_ms.js
+
+============小火箭=========
+京东秒秒币 = type=cron,script-path=https://gitee.com/lxk0301/jd_scripts/raw/master/jd_ms.js, cronexpr="10 7 * * *", timeout=200, enable=true
  */
 const $ = new Env('京东秒秒币');
 
