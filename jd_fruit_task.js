4a04b04ea5b136e994219f9eecd95ab5b29a9bc0
gys619 <1554537386@qq.com>
2022-02-19 23:46:12 +0800

    修改内容：[ jd_fruit_task.js]

--- jd_fruit_task.js
+++ jd_fruit_task.js
@@ -50,7 +50,6 @@ const retainWater = $.isNode() ? (process.env.retainWater ? process.env.retainWa
 let jdNotify = false; //是否关闭通知，false打开通知推送，true关闭通知推送
 let jdFruitBeanCard = false; //农场使用水滴换豆卡(如果出现限时活动时100g水换20豆,此时比浇水划算,推荐换豆),true表示换豆(不浇水),false表示不换豆(继续浇水),脚本默认是浇水
 let randomCount = $.isNode() ? 20 : 5;
-let fruit_sleep = '20000';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 const urlSchema = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://h5.m.jd.com/babelDiy/Zeus/3KSjXqQabiTuD1cJ28QskrpWoBKT/index.html%22%20%7D`;
 !(async() => {
