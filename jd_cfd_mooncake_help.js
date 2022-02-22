ef1ea006a20381f4d38f396d4ae555a6d23dc6d8
gys619 <1554537386@qq.com>
2022-02-22 18:15:21 +0800

    修改内容：[ jd_cfd_mooncake_help.js]

--- jd_cfd_mooncake_help.js
+++ jd_cfd_mooncake_help.js
@@ -38,7 +38,6 @@ $.result = [];
 $.shareCodes = [];
 let cookiesArr = [], cookie = '', token = '';
 let UA, UAInfo = {};
-let nowTimes;
 const randomCount = $.isNode() ? 20 : 3;
 $.appId = "92a36";
 if ($.isNode()) {
@@ -116,7 +115,6 @@ if ($.isNode()) {
 
 async function cfd() {
   try {
-    nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000)
     let beginInfo = await getUserInfo();
     if (beginInfo.LeadInfo.dwLeadType === 2) {
       console.log(`还未开通活动，尝试初始化`)
@@ -131,6 +129,10 @@ async function cfd() {
       }
     }
 
+    if (!beginInfo.MarkList.daily_task_win) {
+      await setMark()
+    }
+	
     //抽奖
     //await $.wait(2000)
     //await composePearlState(4)
@@ -461,6 +463,25 @@ function getAuthorShareCode(url) {
   })
 }
 
+function setMark() {
+  return new Promise(resolve => {
+    $.get(taskUrl("user/SetMark", `strMark=daily_task_win&strValue=1&dwType=1`), (err, resp, data) => {
+      try {
+        if (err) {
+          console.log(`${JSON.stringify(err)}`);
+          console.log(`${$.name} SetMark API请求失败，请检查网路重试`);
+        } else {
+          data = JSON.parse(data.replace(/\n/g, "").match(new RegExp(/jsonpCBK.?\((.*);*\)/))[1]);
+        }
+      } catch (e) {
+        $.logErr(e, resp);
+      } finally{
+        resolve();
+      }
+    })
+  })
+}
+
 // 获取用户信息
 function getUserInfo(showInvite = true) {
   return new Promise(async (resolve) => {
@@ -479,6 +500,7 @@ function getUserInfo(showInvite = true) {
             dwLandLvl,
             LeadInfo = {},
             Business = {},
+            MarkList = {}
           } = data;
           if (showInvite) {
             console.log(`获取用户信息：${sErrMsg}\n${$.showLog ? data : ""}`);
@@ -496,12 +518,14 @@ function getUserInfo(showInvite = true) {
             strMyShareId,
             dwLandLvl,
             LeadInfo,
+            MarkList
           };
           resolve({
             ddwRichBalance,
             ddwCoinBalance,
             strMyShareId,
             LeadInfo,
+            MarkList
           });
         }
       } catch (e) {
