923b796d6fb95c550cab36c1574e4f8b12a48d03
gys619 <1554537386@qq.com>
2022-02-26 01:38:50 +0800

    修改内容：[ jd_cfd.js]

--- jd_cfd.js
+++ jd_cfd.js
@@ -38,8 +38,9 @@ $.result = [];
 $.shareCodes = [];
 let cookiesArr = [], cookie = '', token = '';
 let UA, UAInfo = {};
+let nowTimes;
 const randomCount = $.isNode() ? 20 : 3;
-$.appId = "92a36";
+$.appId = 10032;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item])
@@ -120,6 +121,7 @@ if ($.isNode()) {
 
 async function cfd() {
   try {
+    nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000)
     let beginInfo = await getUserInfo();
     if (beginInfo.LeadInfo.dwLeadType === 2) {
       console.log(`还未开通活动，尝试初始化`)
@@ -132,10 +134,6 @@ async function cfd() {
         console.log(`初始化失败\n`)
         return
       }
-    }
-
-    if (!beginInfo.MarkList.daily_task_win) {
-      await setMark()
     }
 
     // 寻宝
@@ -1123,25 +1121,6 @@ function getAuthorShareCode(url) {
   })
 }
 
-function setMark() {
-  return new Promise(resolve => {
-    $.get(taskUrl("user/SetMark", `strMark=daily_task_win&strValue=1&dwType=1`), (err, resp, data) => {
-      try {
-        if (err) {
-          console.log(`${JSON.stringify(err)}`);
-          console.log(`${$.name} SetMark API请求失败，请检查网路重试`);
-        } else {
-          data = JSON.parse(data.replace(/\n/g, "").match(new RegExp(/jsonpCBK.?\((.*);*\)/))[1]);
-        }
-      } catch (e) {
-        $.logErr(e, resp);
-      } finally{
-        resolve();
-      }
-    })
-  })
-}
-
 // 获取用户信息
 function getUserInfo(showInvite = true) {
   return new Promise(async (resolve) => {
@@ -1163,8 +1142,7 @@ function getUserInfo(showInvite = true) {
             LeadInfo = {},
             StoryInfo = {},
             Business = {},
-            XbStatus = {},
-            MarkList = {}
+            XbStatus = {}
           } = data;
           if (showInvite) {
             console.log(`获取用户信息：${sErrMsg}\n${$.showLog ? data : ""}`);
@@ -1185,8 +1163,7 @@ function getUserInfo(showInvite = true) {
             dwLandLvl,
             LeadInfo,
             StoryInfo,
-            XbStatus,
-            MarkList
+            XbStatus
           };
           resolve({
             buildInfo,
@@ -1195,8 +1172,7 @@ function getUserInfo(showInvite = true) {
             strMyShareId,
             LeadInfo,
             StoryInfo,
-            XbStatus,
-            MarkList
+            XbStatus
           });
         }
       } catch (e) {
@@ -1740,7 +1716,7 @@ async function requestAlgo() {
       'Accept-Language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en;q=0.7'
     },
     'body': JSON.stringify({
-      "version": "3.0",
+      "version": "1.0",
       "fp": $.fingerprint,
       "appId": $.appId.toString(),
       "timestamp": Date.now(),
