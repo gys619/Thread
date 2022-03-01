91f6bb577a690a589b64123da94cad7f93aa5148
gys619 <1554537386@qq.com>
2022-03-01 11:45:20 +0800

    修改内容：[ activity/jd_nzmh.js] 修改文件名：[ jd_10_4.js -> activity/jd_10_4.js, jd_15_5.js -> activity/jd_15_5.js, jd_29_8.js -> activity/jd_29_8.js, jd_5_2.js -> activity/jd_5_2.js]

--- activity/jd_nzmh.js
+++ activity/jd_nzmh.js
@@ -38,8 +38,8 @@ if ($.isNode()) {
     return;
   }
   console.log('女装盲盒\n' +
-      '活动时间：2022-1-1至2022-1-31\n' +
-      '活动地址：https://anmp.jd.com/babelDiy/Zeus/293owQqRtKfS8cuUhk4YzGFYGw2W/index.html');
+      '活动时间：2022-1-29至2022-2-28\n' +
+      '活动地址：https://anmp.jd.com/babelDiy/Zeus/3ZHWXfEDpu5GyX1BgCEN3qQwrC4K/index.html');
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
@@ -59,7 +59,7 @@ if ($.isNode()) {
         continue
       }
       try {
-        await jdMh('https://anmp.jd.com/babelDiy/Zeus/293owQqRtKfS8cuUhk4YzGFYGw2W/index.html')
+        await jdMh('https://anmp.jd.com/babelDiy/Zeus/3ZHWXfEDpu5GyX1BgCEN3qQwrC4K/index.html')
       } catch (e) {
         $.logErr(e)
       }
