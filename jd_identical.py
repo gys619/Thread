ef069e19e2ebaa96f8aa74fc3140b1d0073321a2
gys619 <1554537386@qq.com>
2022-03-24 15:39:25 +0800

    修改内容：[ jd_identical.py, jd_identicalnew.py]

--- jd_identical.py
+++ jd_identical.py
@@ -28,7 +28,7 @@ if not ipport:
     ipport = "localhost:5700"
 else:
     ipport = ipport.lstrip("http://").rstrip("/")
-sub_str = os.getenv("RES_SUB", "gys619_jdd")
+sub_str = os.getenv("RES_SUB", "gys619_Absinthe")
 sub_list = sub_str.split("&")
 res_only = os.getenv("RES_ONLY", True)
 headers = {
