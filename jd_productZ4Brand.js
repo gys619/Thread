f632e9c751f807edb48733e740c69f4e1b0f0f13
gys619 <1554537386@qq.com>
2022-04-24 08:21:27 +0800

    修改内容：[ jd_productZ4Brand.js]

--- jd_productZ4Brand.js
+++ jd_productZ4Brand.js
@@ -1,7 +1,7 @@
 /*
  特务集卡
  脚本没有自动开卡，会尝试领取开卡奖励
-cron 23 9,10,18,20 23 4 * https://raw.githubusercontent.com/333333/jd/main/scripts/jd_productZ4Brand.js
+cron 23 10,18,20 24 4 * https://raw.githubusercontent.com/333333/jd/main/scripts/jd_productZ4Brand.js
 * */
 const $ = new Env('特务');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
