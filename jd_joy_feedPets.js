901262de7d3fb3965a018a952a0e48d7522f8d57
gys619 <1554537386@qq.com>
2022-05-25 18:24:00 +0800

    删除：[ QuantumultX/cookies.conf, QuantumultX/gallery.json, Surge/Task.sgmodule, backUp/jd_cfd_SlotMachine.js, backUp/jd_cfdtx.js, backUp/jd_ddPlayer.js, backUp/jd_decompression.js, backUp/jd_hyj.js, backUp/jd_industryLottery.js, backUp/jd_jika.js, backUp/jd_necklace_new.js, backUp/jd_phone.js, backUp/jd_superBrand2.js, backUp/jd_teamAnJia.js, backUp/jd_travel_shop.js, backUp/jd_xxy.js, backUp/jd_ys.js, boxjs.json, jd_cash_exchange.js, jd_daily_egg.js, jd_family.js, jd_fcwb.js, jd_getFollowGift.py, jd_joy_run.js, jd_joy_steal.js, jd_joypark_open.js, jd_jxg.js, jd_jxnc.js, jd_lotteryMachine.js, jd_market_lottery.js, jd_refreshInvokeKey.js, jd_speed.js, jd_upgrade.js, jd_week.js, joy_run_token.json, tools/a.py, tools/empty.json, tools/jd_dreamFactory_product.js, utils/JDCookie.py, utils/MovementFaker.js, utils/jdCookie.js] 修改内容：[ backUp/jd_big_winner.js, backUp/jd_crazy_joy.js, backUp/jd_crazy_joy_bonus.js, backUp/jd_crazy_joy_coin.js, backUp/jd_price.js, backUp/jd_teamFLP.js, backUp/jd_xtg.js, getJDCookie.js, jd_daily_lottery.js, jd_ddnc_farmpark.js, jd_hbCount.py, jd_jieMo.js, jd_joy_feedPets.js, jd_jump.js, jd_kd.js, jd_mohe.js]

--- jd_joy_feedPets.js
+++ jd_joy_feedPets.js
@@ -7,31 +7,25 @@
 ==============Quantumult X==============
 [task_local]
 #京东宠汪汪喂食
-15 0-23/1 * * * jd_joy_feedPets.js, tag=京东宠汪汪喂食, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdcww.png, enabled=true
+15 0-23/1 * * * https://raw.githubusercontent.com/222222/sync/jd_scripts/jd_joy_feedPets.js, tag=京东宠汪汪喂食, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdcww.png, enabled=true
 
 ==============Loon===============
 [Script]
-cron "15 0-23/1 * * *" script-path=jd_joy_feedPets.js,tag=京东宠汪汪喂食
+cron "15 0-23/1 * * *" script-path=https://raw.githubusercontent.com/222222/sync/jd_scripts/jd_joy_feedPets.js,tag=京东宠汪汪喂食
 
 =========Surge=============
 [Script]
-京东宠汪汪喂食 = type=cron,cronexp="15 0-23/1 * * *",wake-system=1,timeout=3600,script-path=jd_joy_feedPets.js
+京东宠汪汪喂食 = type=cron,cronexp="15 0-23/1 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/222222/sync/jd_scripts/jd_joy_feedPets.js
 
 ===============小火箭==========
-京东宠汪汪喂食 = type=cron,script-path=jd_joy_feedPets.js, cronexpr="15 0-23/1 * * *", timeout=3600, enable=true
+京东宠汪汪喂食 = type=cron,script-path=https://raw.githubusercontent.com/222222/sync/jd_scripts/jd_joy_feedPets.js, cronexpr="15 0-23/1 * * *", timeout=3600, enable=true
 */
 const $ = new Env('宠汪汪🐕喂食');
-const zooFaker = require('./utils/JDJRValidator_Pure');
+const zooFaker = require('./JDJRValidator_Aaron');
 $.get = zooFaker.injectToRequest2($.get.bind($));
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let invoke_key = "RtKLB8euDo7KwsO0";
-try{
-    let hConfig = require('./utils/HConfig.js')
-    invoke_key = hConfig.invokeKey
-}catch(e){}
-
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 if ($.isNode()) {
@@ -46,7 +40,6 @@ let jdNotify = true;//是否开启静默运行。默认true开启
 let message = '', subTitle = '';
 const JD_API_HOST = 'https://jdjoy.jd.com'
 let FEED_NUM = ($.getdata('joyFeedCount') * 1) || 10;   //喂食数量默认10g,可选 10,20,40,80 , 其他数字不可.
-
 !(async () => {
   if (!cookiesArr[0]) {
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
@@ -60,16 +53,16 @@ let FEED_NUM = ($.getdata('joyFeedCount') * 1) || 10;   //喂食数量默认10g,
       $.isLogin = true;
       $.nickName = '';
       await $.wait(100);
-      // await TotalBean();
+      await TotalBean();
       console.log(`\n*****开始【京东账号${$.index}】${$.nickName || $.UserName}******\n`);
-      // if (!$.isLogin) {
-      //   $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
-      //
-      //   if ($.isNode()) {
-      //     await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
-      //   }
-      //   continue
-      // }
+      if (!$.isLogin) {
+        $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
+      
+        if ($.isNode()) {
+          await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
+        }
+        continue
+      }
       message = '';
       subTitle = '';
       if ($.isNode()) {
@@ -82,7 +75,7 @@ let FEED_NUM = ($.getdata('joyFeedCount') * 1) || 10;   //喂食数量默认10g,
         }
       }
       $.validate = '';
-      // const zooFaker = require('./utils/JDJRValidator_Pure');
+      // const zooFaker = require('./JDJRValidator_Aaron');
       // $.validate = await zooFaker.injectToRequest()
       await feedPets(FEED_NUM);//喂食
       await ThreeMeals();//三餐
@@ -108,31 +101,22 @@ function feedPets(feedNum) {
     console.log(`您设置的喂食数量::${FEED_NUM}g\n`);
     if (FEED_NUM === 0) { console.log(`跳出喂食`);resolve();return }
     console.log(`实际的喂食数量::${feedNum}g\n`);
-    let opt = {
-      url: `//jdjoy.jd.com/common/pet/feed?feedCount=${feedNum}&reqSource=h5&invokeKey=${invoke_key}`,
-      // url: "//draw.jdfcloud.com/common/pet/getPetTaskConfig?reqSource=h5&invokeKey=${invoke_key}",
-      method: "GET",
-      data: {},
-      credentials: "include",
-      header: {"content-type": "application/json"}
-    }
-    const url = "https:"+ taroRequest(opt)['url'] + $.validate;
+    const url = `https://jdjoy.jd.com/common/pet/feed?feedCount=${feedNum}&reqSource=h5&invokeKey=q8DNJdpcfRQ69gIx` + $.validate;
     let lkt = new Date().getTime()
-    let lks = $.md5('' + invoke_key + lkt).toString()
+    let lks = $.md5('' + 'q8DNJdpcfRQ69gIx' + lkt).toString()
     const options = {
       url,
       headers: {
-        'Cookie': cookie,
-        'reqSource': 'h5',
-        'Host': 'jdjoy.jd.com',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/json',
-        'Referer': 'https://jdjoy.jd.com/pet/index',
-        'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
-        'Accept-Language': 'zh-cn',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'lkt': lkt,
-        'lks': lks
+        "Host": "jdjoy.jd.com",
+        "Accept": "*/*",
+        "Origin": "https://h5.m.jd.com",
+        "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+        "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
+        "Referer": "https://h5.m.jd.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Cookie": cookie,
+        "lkt": lkt,
+        "lks": lks
       }
     }
     $.get(options, async (err, resp, data) => {
@@ -181,31 +165,22 @@ function feedPets(feedNum) {
 //三餐
 function ThreeMeals() {
   return new Promise(resolve => {
-    let opt = {
-      url: `//jdjoy.jd.com/common/pet/getFood?taskType=ThreeMeals&reqSource=h5&invokeKey=${invoke_key}`,
-      // url: "//draw.jdfcloud.com/common/pet/getPetTaskConfig?reqSource=h5&invokeKey=${invoke_key}",
-      method: "GET",
-      data: {},
-      credentials: "include",
-      header: {"content-type": "application/json"}
-    }
-    const url = "https:"+ taroRequest(opt)['url'] + $.validate;
+    const url = `https://jdjoy.jd.com/common/pet/getFood?taskType=ThreeMeals&reqSource=h5&invokeKey=q8DNJdpcfRQ69gIx` + $.validate
     let lkt = new Date().getTime()
-    let lks = $.md5('' + invoke_key + lkt).toString()
+    let lks = $.md5('' + 'q8DNJdpcfRQ69gIx' + lkt).toString()
     const options = {
       url,
       headers: {
-        'Cookie': cookie,
-        'reqSource': 'h5',
-        'Host': 'jdjoy.jd.com',
-        'Connection': 'keep-alive',
-        'Content-Type': 'application/json',
-        'Referer': 'https://jdjoy.jd.com/pet/index',
-        'User-Agent': $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
-        'Accept-Language': 'zh-cn',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'lkt': lkt,
-        'lks': lks
+        "Host": "jdjoy.jd.com",
+        "Accept": "*/*",
+        "Origin": "https://h5.m.jd.com",
+        "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+        "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1"),
+        "Referer": "https://h5.m.jd.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Cookie": cookie,
+        "lkt": lkt,
+        "lks": lks
       }
     }
     $.get(options, async (err, resp, data) => {
@@ -237,154 +212,43 @@ function jsonParse(str) {
   }
 }
 function TotalBean() {
-  return new Promise(async resolve => {
+  return new Promise(resolve => {
     const options = {
-      "url": `https://wq.jd.com/user/info/QueryJDUserInfo?sceneval=2`,
-      "headers": {
-        "Accept": "application/json,text/plain, */*",
-        "Content-Type": "application/x-www-form-urlencoded",
+      url: "https://me-api.jd.com/user_new/info/GetJDUserInfoUnion",
+      headers: {
+        "Host": "me-api.jd.com",
+        "Accept": "*/*",
+        "User-Agent": "ScriptableWidgetExtension/185 CFNetwork/1312 Darwin/21.0.0",
+        "Accept-Language": "zh-CN,zh-Hans;q=0.9",
         "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-cn",
-        "Connection": "keep-alive",
-        "Cookie": cookie,
-        "Referer": "https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2",
-        "User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
+        "Cookie": cookie
       }
     }
-    $.post(options, (err, resp, data) => {
+    $.get(options, (err, resp, data) => {
       try {
         if (err) {
-          console.log(`${JSON.stringify(err)}`)
-          console.log(`${$.name} API请求失败，请检查网路重试`)
+          $.logErr(err)
         } else {
           if (data) {
             data = JSON.parse(data);
-            if (data['retcode'] === 13) {
+            if (data['retcode'] === "1001") {
               $.isLogin = false; //cookie过期
-              return
+              return;
             }
-            if (data['retcode'] === 0) {
-              $.nickName = (data['base'] && data['base'].nickname) || $.UserName;
-            } else {
-              $.nickName = $.UserName
+            if (data['retcode'] === "0" && data.data && data.data.hasOwnProperty("userInfo")) {
+              $.nickName = data.data.userInfo.baseInfo.nickname;
             }
           } else {
-            console.log(`京东服务器返回空数据`)
+            console.log('京东服务器返回空数据');
           }
         }
       } catch (e) {
         $.logErr(e, resp)
       } finally {
-        resolve();
+        resolve()
       }
     })
   })
-}
-function taroRequest(e) {
-  const a = $.isNode() ? require('crypto-js') : CryptoJS;
-  const i = "98c14c997fde50cc18bdefecfd48ceb7"
-  const o = a.enc.Utf8.parse(i)
-  const r = a.enc.Utf8.parse("ea653f4f3c5eda12");
-  let _o = {
-    "AesEncrypt": function AesEncrypt(e) {
-      var n = a.enc.Utf8.parse(e);
-      return a.AES.encrypt(n, o, {
-        "iv": r,
-        "mode": a.mode.CBC,
-        "padding": a.pad.Pkcs7
-      }).ciphertext.toString()
-    },
-    "AesDecrypt": function AesDecrypt(e) {
-      var n = a.enc.Hex.parse(e)
-          , t = a.enc.Base64.stringify(n);
-      return a.AES.decrypt(t, o, {
-        "iv": r,
-        "mode": a.mode.CBC,
-        "padding": a.pad.Pkcs7
-      }).toString(a.enc.Utf8).toString()
-    },
-    "Base64Encode": function Base64Encode(e) {
-      var n = a.enc.Utf8.parse(e);
-      return a.enc.Base64.stringify(n)
-    },
-    "Base64Decode": function Base64Decode(e) {
-      return a.enc.Base64.parse(e).toString(a.enc.Utf8)
-    },
-    "Md5encode": function Md5encode(e) {
-      return a.MD5(e).toString()
-    },
-    "keyCode": "98c14c997fde50cc18bdefecfd48ceb7"
-  }
-
-  const c = function sortByLetter(e, n) {
-    if (e instanceof Array) {
-      n = n || [];
-      for (var t = 0; t < e.length; t++)
-        n[t] = sortByLetter(e[t], n[t])
-    } else
-      !(e instanceof Array) && e instanceof Object ? (n = n || {},
-          Object.keys(e).sort().map(function(t) {
-            n[t] = sortByLetter(e[t], n[t])
-          })) : n = e;
-    return n
-  }
-  const s = function isInWhiteAPI(e) {
-    for (var n =  ["gift", "pet"], t = !1, a = 0; a < n.length; a++) {
-      var i = n[a];
-      e.includes(i) && !t && (t = !0)
-    }
-    return t
-  }
-
-  const d = function addQueryToPath(e, n) {
-    if (n && Object.keys(n).length > 0) {
-      var t = Object.keys(n).map(function(e) {
-        return e + "=" + n[e]
-      }).join("&");
-      return e.indexOf("?") >= 0 ? e + "&" + t : e + "?" + t
-    }
-    return e
-  }
-  const l = function apiConvert(e) {
-    for (var n = r, t = 0; t < n.length; t++) {
-      var a = n[t];
-      e.includes(a) && !e.includes("common/" + a) && (e = e.replace(a, "common/" + a))
-    }
-    return e
-  }
-
-  var n = e
-      , t = (n.header,
-      n.url);
-  t += (t.indexOf("?") > -1 ? "&" : "?") + "reqSource=h5";
-  var _a = function getTimeSign(e) {
-    var n = e.url
-        , t = e.method
-        , a = void 0 === t ? "GET" : t
-        , i = e.data
-        , r = e.header
-        , m = void 0 === r ? {} : r
-        , p = a.toLowerCase()
-        , g = _o.keyCode
-        , f = m["content-type"] || m["Content-Type"] || ""
-        , h = ""
-        , u = +new Date();
-    return h = "get" !== p &&
-    ("post" !== p || "application/x-www-form-urlencoded" !== f.toLowerCase() && i && Object.keys(i).length) ?
-        _o.Md5encode(_o.Base64Encode(_o.AesEncrypt("" + JSON.stringify(c(i)))) + "_" + g + "_" + u) :
-        _o.Md5encode("_" + g + "_" + u),
-    s(n) && (n = d(n, {
-      "lks": h,
-      "lkt": u
-    }),
-        n = l(n)),
-        Object.assign(e, {
-          "url": n
-        })
-  }(e = Object.assign(e, {
-    "url": t
-  }));
-  return _a
 }
 // md5
 !function(n){function t(n,t){var r=(65535&n)+(65535&t);return(n>>16)+(t>>16)+(r>>16)<<16|65535&r}function r(n,t){return n<<t|n>>>32-t}function e(n,e,o,u,c,f){return t(r(t(t(e,n),t(u,f)),c),o)}function o(n,t,r,o,u,c,f){return e(t&r|~t&o,n,t,u,c,f)}function u(n,t,r,o,u,c,f){return e(t&o|r&~o,n,t,u,c,f)}function c(n,t,r,o,u,c,f){return e(t^r^o,n,t,u,c,f)}function f(n,t,r,o,u,c,f){return e(r^(t|~o),n,t,u,c,f)}function i(n,r){n[r>>5]|=128<<r%32,n[14+(r+64>>>9<<4)]=r;var e,i,a,d,h,l=1732584193,g=-271733879,v=-1732584194,m=271733878;for(e=0;e<n.length;e+=16){i=l,a=g,d=v,h=m,g=f(g=f(g=f(g=f(g=c(g=c(g=c(g=c(g=u(g=u(g=u(g=u(g=o(g=o(g=o(g=o(g,v=o(v,m=o(m,l=o(l,g,v,m,n[e],7,-680876936),g,v,n[e+1],12,-389564586),l,g,n[e+2],17,606105819),m,l,n[e+3],22,-1044525330),v=o(v,m=o(m,l=o(l,g,v,m,n[e+4],7,-176418897),g,v,n[e+5],12,1200080426),l,g,n[e+6],17,-1473231341),m,l,n[e+7],22,-45705983),v=o(v,m=o(m,l=o(l,g,v,m,n[e+8],7,1770035416),g,v,n[e+9],12,-1958414417),l,g,n[e+10],17,-42063),m,l,n[e+11],22,-1990404162),v=o(v,m=o(m,l=o(l,g,v,m,n[e+12],7,1804603682),g,v,n[e+13],12,-40341101),l,g,n[e+14],17,-1502002290),m,l,n[e+15],22,1236535329),v=u(v,m=u(m,l=u(l,g,v,m,n[e+1],5,-165796510),g,v,n[e+6],9,-1069501632),l,g,n[e+11],14,643717713),m,l,n[e],20,-373897302),v=u(v,m=u(m,l=u(l,g,v,m,n[e+5],5,-701558691),g,v,n[e+10],9,38016083),l,g,n[e+15],14,-660478335),m,l,n[e+4],20,-405537848),v=u(v,m=u(m,l=u(l,g,v,m,n[e+9],5,568446438),g,v,n[e+14],9,-1019803690),l,g,n[e+3],14,-187363961),m,l,n[e+8],20,1163531501),v=u(v,m=u(m,l=u(l,g,v,m,n[e+13],5,-1444681467),g,v,n[e+2],9,-51403784),l,g,n[e+7],14,1735328473),m,l,n[e+12],20,-1926607734),v=c(v,m=c(m,l=c(l,g,v,m,n[e+5],4,-378558),g,v,n[e+8],11,-2022574463),l,g,n[e+11],16,1839030562),m,l,n[e+14],23,-35309556),v=c(v,m=c(m,l=c(l,g,v,m,n[e+1],4,-1530992060),g,v,n[e+4],11,1272893353),l,g,n[e+7],16,-155497632),m,l,n[e+10],23,-1094730640),v=c(v,m=c(m,l=c(l,g,v,m,n[e+13],4,681279174),g,v,n[e],11,-358537222),l,g,n[e+3],16,-722521979),m,l,n[e+6],23,76029189),v=c(v,m=c(m,l=c(l,g,v,m,n[e+9],4,-640364487),g,v,n[e+12],11,-421815835),l,g,n[e+15],16,530742520),m,l,n[e+2],23,-995338651),v=f(v,m=f(m,l=f(l,g,v,m,n[e],6,-198630844),g,v,n[e+7],10,1126891415),l,g,n[e+14],15,-1416354905),m,l,n[e+5],21,-57434055),v=f(v,m=f(m,l=f(l,g,v,m,n[e+12],6,1700485571),g,v,n[e+3],10,-1894986606),l,g,n[e+10],15,-1051523),m,l,n[e+1],21,-2054922799),v=f(v,m=f(m,l=f(l,g,v,m,n[e+8],6,1873313359),g,v,n[e+15],10,-30611744),l,g,n[e+6],15,-1560198380),m,l,n[e+13],21,1309151649),v=f(v,m=f(m,l=f(l,g,v,m,n[e+4],6,-145523070),g,v,n[e+11],10,-1120210379),l,g,n[e+2],15,718787259),m,l,n[e+9],21,-343485551),l=t(l,i),g=t(g,a),v=t(v,d),m=t(m,h)}return[l,g,v,m]}function a(n){var t,r="",e=32*n.length;for(t=0;t<e;t+=8){r+=String.fromCharCode(n[t>>5]>>>t%32&255)}return r}function d(n){var t,r=[];for(r[(n.length>>2)-1]=void 0,t=0;t<r.length;t+=1){r[t]=0}var e=8*n.length;for(t=0;t<e;t+=8){r[t>>5]|=(255&n.charCodeAt(t/8))<<t%32}return r}function h(n){return a(i(d(n),8*n.length))}function l(n,t){var r,e,o=d(n),u=[],c=[];for(u[15]=c[15]=void 0,o.length>16&&(o=i(o,8*n.length)),r=0;r<16;r+=1){u[r]=909522486^o[r],c[r]=1549556828^o[r]}return e=i(u.concat(d(t)),512+8*t.length),a(i(c.concat(e),640))}function g(n){var t,r,e="";for(r=0;r<n.length;r+=1){t=n.charCodeAt(r),e+="0123456789abcdef".charAt(t>>>4&15)+"0123456789abcdef".charAt(15&t)}return e}function v(n){return unescape(encodeURIComponent(n))}function m(n){return h(v(n))}function p(n){return g(m(n))}function s(n,t){return l(v(n),v(t))}function C(n,t){return g(s(n,t))}function A(n,t,r){return t?r?s(t,n):C(t,n):r?m(n):p(n)}$.md5=A}(this);
