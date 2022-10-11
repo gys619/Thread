bac4a30c0df47de2d275580fe9d2f87f31bc700f
gys619 <1554537386@qq.com>
2022-10-11 08:40:37 +0800

    up

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -542,9 +542,9 @@ function tjdoublesign(url){
   let opt = {
     url,
     headers: {
-      'accept':'application/json',
-      'referer': 'https://wqs.jd.com/',
-      'User-Agent': 'jdapp;iPhone;4.13.0;14.4.2;${randomString(40)};network/wifi;model/iPhone10,2;appBuild/100609;supportApplePay/1;hasUPPay/0;pushNoticeIsOpen/1;hasOCPay/0;supportBestPay/0;session/${Math.random * 98 + 1};pap/JA2019_3111789;brand/apple;supportJDSHWK/1;Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+      'Accept':'application/json',
+      'Referer': 'https://wqs.jd.com/',
+      'User-Agent': 'jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
       'Cookie': cookie
     }
   }
