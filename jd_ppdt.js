8994ede1463a0634c0b6368c87c5a5a19a9e2060
gys619 <1554537386@qq.com>
2023-01-15 12:10:47 +0800

    up

--- jd_ppdt.js
+++ jd_ppdt.js
@@ -1,6 +1,6 @@
 
 /*
-18 22 * * * jd_ppdt.js
+18 10 * * * jd_ppdt.js
  */
 
 const $ = new Env('超级品牌殿堂');
@@ -42,7 +42,10 @@ if ($.isNode()) {
             await xxx1();
             await $.wait(500);
             await xxx2();
+            await $.wait(500);
+            await xxx3();                   
             await $.wait(2000);
+
         }
     }
 })()
@@ -141,7 +144,49 @@ async function xxx2() {
     })
 }
 
-
+async function xxx3() {
+    let opt = {
+        url: `https://api.m.jd.com/?client=wh5&appid=ProductZ4Brand&functionId=superBrandDoTask&t=1673746202042&body=%7B%22source%22:%22hall_1111%22,%22activityId%22:1012353,%22completionFlag%22:1,%22encryptProjectId%22:%22mCqqcvGW1LKeAWqJtc6NwHGXK2u%22,%22encryptAssignmentId%22:%22H8VttZkAwM83dpETucHznqaNGAc%22,%22assignmentType%22:0,%22actionType%22:0%7D`,
+        headers: {
+            'Origin': 'https://prodev.m.jd.com',
+            'User-Agent': $.UA,
+            'Cookie': cookie
+        }
+    }
+    return new Promise(async (resolve) => {
+        $.post(opt, async (err, resp, data) => {
+            try {
+                if (err) {
+                    console.log(`${JSON.stringify(err)}`)
+                    console.log(` API请求失败，请检查网路重试`)
+                } else {
+                    data = JSON.parse(data)
+                    if (data.code == 0) {
+                        if (data.data.bizCode == 0) {
+                            if (data.data?.result?.rewards) {
+                                if (data.data?.result?.rewards[0].awardType === 3) {
+                                    console.log(` 恭喜获得 ${data.data?.result?.rewards[0].beanNum} 京豆`);
+                                } else {
+                                    console.log(JSON.stringify(data.data?.result?.rewards));
+                                }
+                            } else {
+                                console.log(JSON.stringify(data.data?.result));
+                            }
+                        } else {
+                            console.log(data.data.bizMsg);
+                        }
+                    } else {
+                        console.log(data.msg)
+                    }
+                }
+            } catch (e) {
+                $.logErr(e, resp)
+            } finally {
+                resolve(data)
+            }
+        })
+    })
+}
 function TotalBean() {
     return new Promise((resolve) => {
         const options = {
